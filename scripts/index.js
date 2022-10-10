e04a876754644f409d99304fd2f451c4a59cc3c6
Juma Harun <mmbh4run@gmail.com>
2022-10-10 22:38:49 +0300

    updated

--- scripts/index.js
+++ scripts/index.js
@@ -1,48 +1,137 @@
-const plusOneHome = document.getElementById("plus1home")
-const plusTwoHome = document.getElementById("plus2home")
-const plusThreeHome = document.getElementById("plus3home")
-const plusOneAway = document.getElementById("plus1away")
-const plusTwoAway = document.getElementById("plus2away")
-const plusThreeAway = document.getElementById("plus3away")
-const startGameBtn = document.getElementById("")
-const resetGameBtn = document.getElementById("")
+const homeScoreEl = document.getElementById('homeScore')
+const guestScoreEl = document.getElementById('guestScore')
+const timerEl = document.getElementById('timer')
+const periodEl = document.getElementById('periodText')
+const plusOneHome = document.getElementById('plus-1-home')
+const plusTwoHome = document.getElementById('plus-2-home')
+const plusThreeHome = document.getElementById('plus-3-home')
+const plusOneGuest = document.getElementById('plus-1-guest')
+const plusTwoGuest = document.getElementById('plus-2-guest')
+const plusThreeGuest = document.getElementById('plus-3-guest')
+const start = document.getElementById('startBtn')
+const reset = document.getElementById('resetBtn')
+const checkbox = document.getElementById('test')
 
-let homeScore = document.getElementById("home-score")
-let awayScore = document.getElementById("guest-score")
+let homeScore = 0
+let guestScore = 0
+let mins = 12
+let secs = 0
 
+checkbox.addEventListener('change', test)
 
-plusOneHome.addEventListener("click", function() {
-    addHome(1)
+start.addEventListener('click', () => {
+    countdown( 'timer', mins, secs );
 })
 
-plusTwoHome.addEventListener("click", function() {
-    addHome(2)
+
+
+plusOneHome.addEventListener('click', ()=>{
+    homeScore++
+    
+    setScoreChars(homeScore, homeScoreEl)
 })
 
-plusThreeHome.addEventListener("click", function() {
-    addHome(3)
+plusTwoHome.addEventListener('click', ()=>{
+    homeScore = homeScore + 2
+    
+    setScoreChars(homeScore, homeScoreEl)
 })
 
-plusOneAway.addEventListener("click", function() {
-    addAway(1)
+plusThreeHome.addEventListener('click', ()=>{
+    homeScore = homeScore + 3
+    
+    setScoreChars(homeScore, homeScoreEl)
 })
 
-plusTwoAway.addEventListener("click", function() {
-    addAway(2)
+plusOneGuest.addEventListener('click', ()=>{
+    guestScore++
+    
+    setScoreChars(guestScore, guestScoreEl)
 })
 
-plusThreeAway.addEventListener("click", function() {
-    addAway(3)
+plusTwoGuest.addEventListener('click', ()=>{
+    guestScore = guestScore + 2
+    
+    setScoreChars(guestScore, guestScoreEl)
 })
 
-function addHome(num) {
-    let score = homeScore.textContent
-    score = Number(score) + num
-    homeScore.textContent = score
+plusThreeGuest.addEventListener('click', ()=>{
+    guestScore = guestScore + 3
+    
+    setScoreChars(guestScore, guestScoreEl)
+})
+
+reset.addEventListener('click', ()=>{
+    window.location.reload()
+})
+
+function setPeriod() {
+    if(periodEl.textContent == 1) {
+        periodEl.textContent = 2
+    } else if(periodEl.textContent == 2) {
+        periodEl.textContent = 3
+    } else if(periodEl.textContent == 3) {
+        periodEl.textContent = 4
+    } else {
+        periodEl.textContent = 1
+    }
 }
 
-function addAway(num) {
-    let score = awayScore.textContent
-    score = Number(score) + num
-    awayScore.textContent = score
+
+
+function countdown( elementName, minutes, seconds )
+{
+    let element, endTime, hours, mins, msLeft, time;
+
+    function twoDigits( n )
+    {
+        return n < 10 ? `0${n}` : n;
+    }
+
+    function updateTimer()
+    {
+        msLeft = endTime - (+new Date);
+        if ( msLeft < 1000 ) {
+            element.innerHTML = "00:00";
+            start.disabled = false
+            setTimeout(() => {
+                test()
+                setPeriod()
+            }, 1500);
+        } else {
+            start.disabled = true
+            time = new Date( msLeft );
+            hours = time.getUTCHours();
+            mins = time.getUTCMinutes();
+            element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
+
+            setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
+        }
+    }
+
+    element = document.getElementById( elementName );
+    endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
+    updateTimer();
+}
+
+function setScoreChars(score, scoreEl) {
+    if(score < 10) {
+        scoreEl.textContent = `00${score}`
+    } else if(score < 100) {
+        scoreEl.textContent = `0${score}`
+    } else {
+        scoreEl.textContent = score
+    }
+}
+
+function test() {
+    if(checkbox.checked) {
+        mins = 0
+        secs = 5
+        timerEl.textContent = "0:05"
+    } else if(!checkbox.checked) {
+        mins = 12
+        secs = 0
+        timerEl.textContent = '12:00'
+    }
 }
