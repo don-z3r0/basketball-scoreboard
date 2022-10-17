4e1cba7f27aff121adff9a2a65cada0f5afb1fc2
Juma Harun <mmbh4run@gmail.com>
2022-10-17 19:39:51 +0300

    refactor

--- README.md
+++ README.md
@@ -1,4 +1,49 @@
-# basketball-scoreboard
-simple basketball scoreboard
+# Basketball Scoreboard | Scrimba's Making Websites Interactive Solo Project
 
-- Live Site URL: [Basketball Scoreboard](https://jharun-basketball-scoreboard.netlify.app/)
+This is my solution to the Scrimba's Basketball Scoreboard from the Making Websites Interactive module of [The Frontend Developer Career Path](https://scrimba.com/learn/frontend/). 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Stretch Goals](#stretch-goals)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+![screenshot of Basketball Scoreboard project requirements](./images/Scoreboard-req.png)
+
+### Stretch Goals
+
+![screenshot of Basketball Scoreboard project stretch goals](./images/Scoreboard-stretch-goals.png)
+
+### Screenshot
+
+![screenshot of my solution on desktop](./images/Scoreboard-pc.png)
+
+![](./images/Scoreboard-mobile.png)
+
+### Links
+
+- Solution URL: [GitHub](https://github.com/jumaHarun/oldgram)
+- Live Site URL: [Oldagram on Netlify](https://jharun-oldagram.netlify.app/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+
+## Author
+
+- LinkedIn - [Juma Harun](https://www.linkedin.com/in/jumaHarun)
+- Twitter - [@jumaH4run](https://twitter.com/jumaH4run/)
--- scripts/index.js
+++ scripts/index.js
@@ -1,137 +1,136 @@
-const homeScoreEl = document.getElementById('homeScore')
-const guestScoreEl = document.getElementById('guestScore')
-const timerEl = document.getElementById('timer')
-const periodEl = document.getElementById('periodText')
-const plusOneHome = document.getElementById('plus-1-home')
-const plusTwoHome = document.getElementById('plus-2-home')
-const plusThreeHome = document.getElementById('plus-3-home')
-const plusOneGuest = document.getElementById('plus-1-guest')
-const plusTwoGuest = document.getElementById('plus-2-guest')
-const plusThreeGuest = document.getElementById('plus-3-guest')
-const start = document.getElementById('startBtn')
-const reset = document.getElementById('resetBtn')
-const checkbox = document.getElementById('test')
+const homeScoreEl = document.getElementById("homeScore");
+const guestScoreEl = document.getElementById("guestScore");
+const timerEl = document.getElementById("timer");
+const periodEl = document.getElementById("periodText");
+const plusOneHome = document.getElementById("plus-1-home");
+const plusTwoHome = document.getElementById("plus-2-home");
+const plusThreeHome = document.getElementById("plus-3-home");
+const plusOneGuest = document.getElementById("plus-1-guest");
+const plusTwoGuest = document.getElementById("plus-2-guest");
+const plusThreeGuest = document.getElementById("plus-3-guest");
+const start = document.getElementById("startBtn");
+const reset = document.getElementById("resetBtn");
+const checkbox = document.getElementById("test");
 
-let homeScore = 0
-let guestScore = 0
-let mins = 12
-let secs = 0
+let homeScore = 0;
+let guestScore = 0;
 
-checkbox.addEventListener('change', test)
-
-start.addEventListener('click', () => {
-    countdown( 'timer', mins, secs );
+document.addEventListener('click', (e) =>{
+    if(e.target.id === 'startBtn') {
+      if(checkbox.checked) {
+            countdownTimer(timerEl, 0, 5)
+            start.disabled = true
+        } else {
+            countdownTimer(timerEl, 12, 0)
+            start.disabled = true
+        }
+    } else if(e.target.id === 'plus-1-home') {
+      homeScore++;
+      highlightLeader()
+      setScoreChars(homeScore, homeScoreEl);
+    } else if(e.target.id === 'plus-2-home') {
+      homeScore = homeScore + 2;
+      highlightLeader()
+      setScoreChars(homeScore, homeScoreEl);
+    } else if(e.target.id === 'plus-3-home') {
+      homeScore = homeScore + 3;
+      highlightLeader()
+      setScoreChars(homeScore, homeScoreEl);
+    } else if(e.target.id === 'plus-1-guest') {
+      guestScore++
+      highlightLeader()
+      setScoreChars(guestScore, guestScoreEl);
+    } else if(e.target.id === 'plus-2-guest') {
+      guestScore = guestScore + 2;
+      highlightLeader()
+      setScoreChars(guestScore, guestScoreEl);
+    } else if(e.target.id === 'plus-3-guest') {
+      guestScore = guestScore + 3;
+      highlightLeader()
+      setScoreChars(guestScore, guestScoreEl);
+    }
 })
 
+checkbox.addEventListener("change", test);
 
-
-plusOneHome.addEventListener('click', ()=>{
-    homeScore++
-    
-    setScoreChars(homeScore, homeScoreEl)
-})
-
-plusTwoHome.addEventListener('click', ()=>{
-    homeScore = homeScore + 2
-    
-    setScoreChars(homeScore, homeScoreEl)
-})
-
-plusThreeHome.addEventListener('click', ()=>{
-    homeScore = homeScore + 3
-    
-    setScoreChars(homeScore, homeScoreEl)
-})
-
-plusOneGuest.addEventListener('click', ()=>{
-    guestScore++
-    
-    setScoreChars(guestScore, guestScoreEl)
-})
-
-plusTwoGuest.addEventListener('click', ()=>{
-    guestScore = guestScore + 2
-    
-    setScoreChars(guestScore, guestScoreEl)
-})
-
-plusThreeGuest.addEventListener('click', ()=>{
-    guestScore = guestScore + 3
-    
-    setScoreChars(guestScore, guestScoreEl)
-})
-
-reset.addEventListener('click', ()=>{
+reset.addEventListener("click", () => {
     window.location.reload()
-})
+    // countdownTimer(timerEl, 0, 0)
+    // timerEl.textContent = '12:00'
+    // checkbox.checked = false
+    // homeScore = 0
+    // guestScore = 0
+    // periodEl.textContent = 0
+});
+
+function countdownTimer(timerEl, minutes, seconds) {
+    let endTime, mins, secs, msLeft, time
+    
+    let x = setInterval(() => {
+        msLeft = endTime - +new Date()
+        if(msLeft < 1000) {
+            clearInterval(x);
+            timerEl.textContent = '00:00'
+            setTimeout(() => {
+                timerEl.textContent = checkbox.checked ? '00:05' : '12:00'
+                setPeriod()
+                start.disabled = false
+            }, 1000);
+        } else {
+            time = new Date(msLeft)
+            mins = time.getUTCMinutes()
+            secs = time.getUTCSeconds()
+
+            timerEl.textContent = `${twoDigits(mins)}:${twoDigits(secs)}`
+        }
+    }, 1000);
+
+    endTime = +new Date() + 1000 * (60 * minutes + seconds) + 500
+}
 
 function setPeriod() {
-    if(periodEl.textContent == 1) {
-        periodEl.textContent = 2
-    } else if(periodEl.textContent == 2) {
-        periodEl.textContent = 3
-    } else if(periodEl.textContent == 3) {
-        periodEl.textContent = 4
+    if (periodEl.textContent == 1) {
+      periodEl.textContent = 2;
+    } else if (periodEl.textContent == 2) {
+      periodEl.textContent = 3;
+    } else if (periodEl.textContent == 3) {
+      periodEl.textContent = 4;
     } else {
-        periodEl.textContent = 1
+      periodEl.textContent = 1;
     }
-}
-
-
-
-function countdown( elementName, minutes, seconds )
-{
-    let element, endTime, hours, mins, msLeft, time;
-
-    function twoDigits( n )
-    {
-        return n < 10 ? `0${n}` : n;
-    }
-
-    function updateTimer()
-    {
-        msLeft = endTime - (+new Date);
-        if ( msLeft < 1000 ) {
-            element.innerHTML = "00:00";
-            start.disabled = false
-            setTimeout(() => {
-                test()
-                setPeriod()
-            }, 1500);
-        } else {
-            start.disabled = true
-            time = new Date( msLeft );
-            hours = time.getUTCHours();
-            mins = time.getUTCMinutes();
-            element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
-
-            setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
-        }
-    }
-
-    element = document.getElementById( elementName );
-    endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
-    updateTimer();
-}
-
-function setScoreChars(score, scoreEl) {
-    if(score < 10) {
-        scoreEl.textContent = `00${score}`
-    } else if(score < 100) {
-        scoreEl.textContent = `0${score}`
+  }
+  
+  function setScoreChars(score, scoreEl) {
+    if (score < 10) {
+      scoreEl.textContent = `00${score}`;
+    } else if (score < 100) {
+      scoreEl.textContent = `0${score}`;
     } else {
-        scoreEl.textContent = score
+      scoreEl.textContent = score;
     }
+  }
+  
+  function test() {
+    if (checkbox.checked) {
+      timerEl.textContent = "00:05";
+    } else if (!checkbox.checked) {
+      timerEl.textContent = "12:00";
+    }
+  }
+
+function twoDigits(num) {
+    return num < 10 ? `0${num}` : num
 }
 
-function test() {
-    if(checkbox.checked) {
-        mins = 0
-        secs = 5
-        timerEl.textContent = "0:05"
-    } else if(!checkbox.checked) {
-        mins = 12
-        secs = 0
-        timerEl.textContent = '12:00'
-    }
+function highlightLeader() {
+  if(homeScore === guestScore) {
+    homeScoreEl.style.color = 'limegreen'
+    guestScoreEl.style.color = 'limegreen'
+  }else if(guestScore > homeScore) {
+    homeScoreEl.style.color = 'darkred'
+    guestScoreEl.style.color = 'limegreen'
+  } else if(homeScore > guestScore) {
+    homeScoreEl.style.color = 'limegreen'
+    guestScoreEl.style.color = 'darkred'
+  }
 }
